1f17e80c9a1ca758081a08ff6b60d6ddd0eb3cfc
Bramdv <bramdevroey@gmail.com>
2021-05-19 10:11:48 +0200

    best prediction function in video processing

--- video_processing.py
+++ video_processing.py
@@ -2,6 +2,7 @@
 import pickle
 import numpy as np
 import cv2
+from numpy.core.fromnumeric import cumprod, cumsum
 import pandas as pd
 from preprocessing import extract_features, load_image
 
@@ -11,10 +12,10 @@ with open("model.sav", "rb") as f:
     model = [*model_dict.values()][0]
 
 
-def process_video(file, fps=2):
+def process_video(file, fps=2, show_video=False):
     cap = cv2.VideoCapture(file)
     video_fps = cap.get(cv2.CAP_PROP_FPS)
-    frames_between_predict = int(video_fps / fps)
+    frames_between_predict = int(video_fps / fps) if fps is not None else 1
 
     out = []
 
@@ -28,6 +29,8 @@ def process_video(file, fps=2):
 
         # get body pose
         if ret:
+            if show_video:
+                cv2.imshow("Yoga pose detection", frame)
             pose_features = extract_features(load_image(frame))
             predicted_pose = model.predict_proba(
                 np.atleast_2d([*pose_features.values()])
@@ -36,6 +39,11 @@ def process_video(file, fps=2):
             for key, val in zip(model.classes_, predicted_pose.flatten()):
                 result[key] = val
             out.append(result)
+            best_pred = get_best_predicition(predicted_pose.flatten(), model.classes_)
+            if best_pred:
+                print(
+                    ", ".join(f"{name} ({prob*100:.0f}%)" for name, prob in best_pred)
+                )
 
         else:
             break
@@ -45,6 +53,22 @@ def process_video(file, fps=2):
     return pd.DataFrame(out)
 
 
+def get_best_predicition(proba, labels, thresholt=0.05) -> list:
+    order = np.argsort(proba)[::-1]
+    results = [(labels[order[0]], proba[order[0]])]
+    cum_proba = proba[order[0]]
+    for o in order[1:]:
+        if abs(results[-1][1] - proba[o]) > thresholt:
+            break
+        results.append((labels[o], proba[o]))
+        cum_proba += proba[o]
+
+    if cum_proba > 0.5 and len(results) < 4:
+        return results
+    else:
+        return []
+
+
 if __name__ == "__main__":
     test = process_video("DATA/training data/training recording/minute.mp4")
-    test.to_csv("test_video_results.csv")
+    test.to_csv("test_video_results.csv", index=False)
